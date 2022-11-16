59f2ea38359c2600c73f26c3ffb47b543210b534
Thuần <ngothuan2422001@gmail.com>
2022-11-16 11:50:39 +0700

    add Upload video

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,6 @@ import jwt from 'jsonwebtoken'
 const auth = async (req, res, next) => {
   try {
     const token = req.headers.authorization
-
     let decodedData = jwt.verify(token, process.env.JWT)
 
     req.userId = decodedData?.id
