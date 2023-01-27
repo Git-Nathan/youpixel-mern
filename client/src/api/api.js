59bf5e62604e41516b3baee1ee672cae4c5b40b2
EliasRizon <ngothuan2422001@gmail.com>
2023-01-27 20:34:31 +0700

    fix link backend

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5002' })
+const API = axios.create({ baseURL: 'https://youpixel-api.onrender.com/' })
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem('profile')) {
